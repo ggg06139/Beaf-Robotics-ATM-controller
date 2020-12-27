ac0e41eaed76ca4f78451b2592d6b0ca5e44a4ac
ggg06139 <70440577+ggg06139@users.noreply.github.com>
2020-12-27 19:36:34 +0900

    Update Bear Robotics ATM controller.py

--- Bear Robotics ATM controller.py
+++ Bear Robotics ATM controller.py
@@ -6,23 +6,25 @@ class ATM:
         self.__account_num = account_num
         self.__balance = balance
         self.__account_pin = account_pin
-
+        
     def __repr__(self):
         return f'\n{self.__name}\'s account ({self.__account_num}) has a balance of ${self.__balance}.'
-
+    
+    #Function to replace card insertion
     def discrimination_card(self):
         input_card = input('Please enter your card (Replace with text \'card\') : ')
         while input_card != 'card':
             print('Invalid input. Please re-enter')
-            input_card = input('Please enter your card'
-                               ' (Replace with text \'card\') : ')
-
+            input_card = input('Please enter your card (Replace with text \'card\') : ')
+            
+    #Function to input PIN number
     def discrimination_pin(self):
         input_pin = input('Please enter your PIN : ')
         if self.__account_pin != int(input_pin):
             print('Invalid input. Please re-enter')
             self.discrimination_pin()
 
+    #Function to choice options
     def discrimination_option(self):
         print('Please select an option')
         input_option = input('1.Withdrawal 2.Deposit 3.Balance Inquiry 4.Transfer 5.Exit (Please enter a number) : ')
@@ -39,13 +41,15 @@ class ATM:
         else:
             print('Invalid input. Please re-enter')
             self.discrimination_option()
-
+    
+    #Function to deposit
     def deposit(self):
         deposit_money = int(input('\nPlease enter the amount to deposit : '))
         self.time_sleep()
         self.__balance += deposit_money
         print(f'${deposit_money} has been deposited. The balance is ${self.__balance}')
 
+    #Function to withdrawal
     def withdrawal(self):
         withdrawal_money = int(input('\nPlease enter the amount to withdraw : '))
         if self.__balance < withdrawal_money:
@@ -55,6 +59,7 @@ class ATM:
             self.__balance -= withdrawal_money
             print(f'The balance after the withdrawal of ${withdrawal_money} is ${self.__balance}')
 
+    #Function to transfer
     def transfer(self):
         transfer_account = input('\nPlease enter the account number of the recipient : ')
         self.discrimination_pin()
@@ -70,6 +75,7 @@ class ATM:
         else:
             self.transfer()
 
+    #Function to additional tasks
     def additional_task(self):
         answer = input('Are there any additional tasks? (Y/N): ')
         if answer == 'Y':
@@ -83,6 +89,7 @@ class ATM:
             print('Invalid input. Please re-enter')
             self.additional_task()
 
+    #Function to print receipt
     def receipt(self):
         answer = input('Would you like to get a receipt? (Y/N): ')
         if answer == 'Y':
@@ -99,6 +106,7 @@ class ATM:
 
         print('\nPlease make sure there are no items left')
 
+    #Function to indicate loading
     def time_sleep(self):
         for _ in range(5):
             print('- ',end='')
@@ -108,24 +116,14 @@ class ATM:
 def main(account):
 
     account.discrimination_card()
-
     print('\n')
-
     account.discrimination_pin()
-
     print('\n')
-
     account.discrimination_option()
-
     print('\n')
-
     account.additional_task()
-
     print('\n')
-
     account.receipt()
 
-
 account = ATM('Hwang Seunghyeon', '1234-1234', 1234, 10000)
-
 main(account)
